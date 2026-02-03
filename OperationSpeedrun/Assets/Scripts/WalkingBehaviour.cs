ee1280a0cabd2117a46157b8c118907c40a95a83
Ava-TU <C24384981@myTUDublin.ie>
2026-02-03 11:59:15 +0000

    made a layer for jumping
    will attempt to do the leg mask in this layer

--- OperationSpeedrun/Assets/Scripts/WalkingBehaviour.cs
+++ OperationSpeedrun/Assets/Scripts/WalkingBehaviour.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 
-public class WalkingBehaviour : MonoBehaviour
+public class WalkingBehaviour : StateMachineBehaviour
 {
     private AudioSource[] audio;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        //Get the audio source component
+        audio = animator.GetComponentsInParent<AudioSource> ();
+        //Play the audio at the beginning of its index, 0
+        audio[0].Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        //Stops the audio when the state has been exited
+        audio[0].Stop();
     }
 }
