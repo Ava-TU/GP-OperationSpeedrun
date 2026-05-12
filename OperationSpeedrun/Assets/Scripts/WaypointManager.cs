2b55d846ced872b6433dc42fb55f4e5611dfdc00
Ava-TU <C24384981@myTUDublin.ie>
2026-05-12 13:53:31 +0100

    stuff

--- OperationSpeedrun/Assets/Scripts/WaypointManager.cs
+++ OperationSpeedrun/Assets/Scripts/WaypointManager.cs
@@ -78,36 +78,4 @@ public class WaypointManager : MonoBehaviour
 			Gizmos.DrawSphere(waypoint.position, 0.2f);
 		}
 	}
-
-
-
-	//int nextIndex;
-	//public GameObject[] waypoints;
-
-	//public GameObject NextWaypoint (GameObject current)
-	//{
-		//if (current != null) 
-		//{
-			// Find array index of given waypoint
-			
-			//for (int i = 0; i < waypoints.Length; i++) 
-			//{
-				// Once found calculate next one
-				
-				//if (current == waypoints [i]) 
-				//{
-					// Modulus operator helps to avoid to go out of bounds
-					// And resets to 0 the index count once we reach the end of the array
-					//nextIndex = (i + 1) % waypoints.Length;
-				//}
-			//}
-		//} 
-		//else 
-		//{
-			// Default is first index in array
-			//nextIndex = 0;
-		//}
-		
-		//return waypoints [nextIndex];
-	//}
 }
